519089f3ffd31a98e3e88d42c68112316fe42729
Wush Wu <wush978@gmail.com>
2012-12-28 15:38:23 +0800

    add interp

---
+++ schema/interp.class.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once( __DIR__ . '/../src/Schema.interface.php' );
+
+class interp implements Schema {
+	
+	/**
+	 * 
+	 * @var Parser
+	 */
+	private $parser;
+	
+	/**
+	 * @var PersonDatabase
+	 */
+	private $db;
+	
+	public function __construct(Parser $parser) {
+		$this->parser = $parser;
+		$this->db = $parser->get_db();
+	}
+	
+	public function __invoke(stdClass $json_content) {
+		$people = $json_content->people;
+		foreach($json_content->people as $person) {
+			$this->db->add(
+				$json_content->type, 
+				$this->parser->get_ad(), 
+				$this->parser->get_session(), 
+				$this->parser->get_sitting(), 
+				$this->parser->get_line_num(), 
+				$person,
+				$this->parser);
+		}
+	}
+}
