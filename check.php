749d280fa1f0dec5a8414fae1af13d75794b7377
Wush Wu <wush978@gmail.com>
2013-01-01 17:33:23 +0800

    put check result to check_log.log

--- check.php
+++ check.php
@@ -32,16 +32,29 @@ foreach($people_list as $person) {
 		$md = file($md_path);
 		echo $md[$info['line'] - 1];
 	}
+	if (preg_match('#員委#u', $person)) {
+		$ok = false;
+		echo "person: $person \n"; 
+		$info = report($person);
+		print_r($info);
+		$info = $info[0];
+		$md_path = 'raw/' . search($info['ad'], $info['session'], $info['sitting']);
+		echo "md_path: $md_path \n";
+		$md = file($md_path);
+		echo $md[$info['line'] - 1];
+	}
 }
 
 $filter_out = shell_exec('grep "filter out:" test.log');
 $filter_out = explode("\n", $filter_out);
 require_once(__DIR__ . '/src/PersonDatabase.class.php');
+$check_log = '.';
 foreach($filter_out as $str) {
 	if (preg_match('#' . PersonDatabase::get_rule('title') . '#ui', $str, $matches)) {
-		echo $str . "\n";
+		$check_log .= $str . "\n";
 	}
 }
+file_put_contents('check_log.log', $check_log );
 
 if (!$ok) 
 	exit(1);
