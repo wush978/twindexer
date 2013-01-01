74a3e06fa306e0a56471f8148f7e31ac3d58b1e3
Wush Wu <wush978@gmail.com>
2013-01-01 19:10:51 +0800

    new result

--- check.php
+++ check.php
@@ -17,26 +17,11 @@ function report($person) {
 	return $index[$person];
 }
 
-$index = json_decode(file_get_contents('result/index.json'), true);
-$people_list = json_decode(file_get_contents('result/people-list.json'),true);
-
-$ok = true;
-foreach($people_list as $person) {
-	if (preg_match('#委員#u', $person)) {
+function check_pattern($pattern, $person, &$ok) {
+	if (preg_match("#$pattern#u", $person)) {
 		$ok = false;
-		echo "person: $person \n"; 
+		echo "person: $person \n";
 		$info = report($person);
-		$info = $info[0];
-		$md_path = 'raw/' . search($info['ad'], $info['session'], $info['sitting']);
-		echo "md_path: $md_path \n";
-		$md = file($md_path);
-		echo $md[$info['line'] - 1];
-	}
-	if (preg_match('#員委#u', $person)) {
-		$ok = false;
-		echo "person: $person \n"; 
-		$info = report($person);
-		print_r($info);
 		$info = $info[0];
 		$md_path = 'raw/' . search($info['ad'], $info['session'], $info['sitting']);
 		echo "md_path: $md_path \n";
@@ -45,10 +30,31 @@ foreach($people_list as $person) {
 	}
 }
 
-$filter_out = shell_exec('grep "filter out:" test.log');
-$filter_out = explode("\n", $filter_out);
+function check($index, $people_list, &$ok) {
+	
+	$pattern_list = yaml_parse_file(__DIR__ . '/rule/check.yml');
+	
+	foreach($people_list as $person) {
+		foreach($pattern_list as $pattern) {
+			check_pattern($pattern, $person, $ok);
+		}
+	}
+}
+
+$ok = true;
+
+$index = json_decode(file_get_contents('result/interp.json'), true);
+$people_list = json_decode(file_get_contents('result/people-list.interp.json'),true);
+check($index, $people_list, $ok);
+
+$index = json_decode(file_get_contents('result/exmotion.json'), true);
+$people_list = json_decode(file_get_contents('result/people-list.exmotion.json'),true);
+check($index, $people_list, $ok);
+
 require_once(__DIR__ . '/src/PersonDatabase.class.php');
 $check_log = '.';
+$filter_out = shell_exec('grep "filter out:" test.log');
+$filter_out = explode("\n", $filter_out);
 foreach($filter_out as $str) {
 	if (preg_match('#' . PersonDatabase::get_rule('title') . '#ui', $str, $matches)) {
 		$check_log .= $str . "\n";
@@ -56,5 +62,6 @@ foreach($filter_out as $str) {
 }
 file_put_contents('check_log.log', $check_log );
 
+exit(0);
 if (!$ok) 
 	exit(1);
--- test.php
+++ test.php
@@ -1,7 +1,5 @@
 <?php
 
-$output = 'index.json';
-
 require_once( __DIR__ . '/log4php/src/main/php/Logger.php');
 
 Logger::configure('config.xml');
@@ -24,8 +22,6 @@ $db = new PHPPersonDatabase();
 $parser = new Parser($db, 'interp');
 
 /* debugging block start */
-// $db->filter('')
-// exit(0);
 /* debugging block end */
 
 PHPPersonDatabase::$is_filter = true;
@@ -40,5 +36,17 @@ foreach($index as $index_element) {
 	$parser->set_parser_info($index_element['ad'], $index_element['session'], $index_element['sitting']);
 	$parser->parse("raw/$file_name");
 }
-file_put_contents('result/people-list.json', json_encode($db->list_people(), JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
-file_put_contents('result/' . $output , json_encode($db->get_db(), JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
+file_put_contents('result/people-list.interp.json', json_encode($db->list_people(), JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
+file_put_contents('result/interp.json', json_encode($db->get_db(), JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
+
+$db->set_name_db($db->list_people());
+$db->clean_db();
+$parser = new Parser($db, 'exmotion');
+foreach($index as $index_element) {
+	$file_name = $index_element['file_name'];
+	$id = $index_element['id'];
+	$parser->set_parser_info($index_element['ad'], $index_element['session'], $index_element['sitting']);
+	$parser->parse("raw/$file_name");
+}
+file_put_contents('result/people-list.exmotion.json', json_encode($db->list_people(), JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
+file_put_contents('result/exmotion.json', json_encode($db->get_db(), JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
