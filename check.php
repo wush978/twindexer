c6dff33e7b622e10c55e7bb2d96fc2794811ffe1
Wush Wu <wush978@gmail.com>
2013-01-01 16:20:34 +0800

    used for debugging

---
+++ check.php
@@ -0,0 +1,47 @@
+<?php
+
+require_once( __DIR__ . '/index.php');
+
+$ly_index = get_index();
+function search($ad, $session, $sitting) {
+	global $ly_index;
+	foreach($ly_index as $md_info) {
+		if ($md_info['id'] === "$ad 屆 $session 會期 $sitting") {
+			return $md_info['file_name'];
+		}
+	}
+}
+
+function report($person) {
+	global $index;
+	return $index[$person];
+}
+
+$index = json_decode(file_get_contents('result/index.json'), true);
+$people_list = json_decode(file_get_contents('result/people-list.json'),true);
+
+$ok = true;
+foreach($people_list as $person) {
+	if (preg_match('#委員#u', $person)) {
+		$ok = false;
+		echo "person: $person \n"; 
+		$info = report($person);
+		$info = $info[0];
+		$md_path = 'raw/' . search($info['ad'], $info['session'], $info['sitting']);
+		echo "md_path: $md_path \n";
+		$md = file($md_path);
+		echo $md[$info['line'] - 1];
+	}
+}
+
+$filter_out = shell_exec('grep "filter out:" test.log');
+$filter_out = explode("\n", $filter_out);
+require_once(__DIR__ . '/src/PersonDatabase.class.php');
+foreach($filter_out as $str) {
+	if (preg_match('#' . PersonDatabase::get_rule('title') . '#ui', $str, $matches)) {
+		echo $str . "\n";
+	}
+}
+
+if (!$ok) 
+	exit(1);
---
+++ test.php
@@ -0,0 +1,39 @@
+<?php
+
+$output = 'index.json';
+
+require_once( __DIR__ . '/log4php/src/main/php/Logger.php');
+
+Logger::configure('config.xml');
+
+function my_warning_handler($errno, $errstr) {
+	
+	throw new Exception($errstr);
+}
+
+set_error_handler("my_warning_handler", E_ALL);
+
+require_once( __DIR__ . '/index.php');
+
+$index = get_index();
+
+require_once( __DIR__ . '/src/PHPPersonDatabase.class.php');
+require_once(__DIR__ . '/src/Parser.class.php');
+
+$db = new PHPPersonDatabase();
+$parser = new Parser($db, 'interp');
+
+/* debugging block start */
+// $db->filter('')
+// exit(0);
+/* debugging block end */
+
+PHPPersonDatabase::$is_filter = true;
+$index = array(array('file_name' => '3599.md', 'id' => 123));
+foreach($index as $index_element) {
+	$file_name = $index_element['file_name'];
+	$id = $index_element['id'];
+	$parser->parse("raw/$file_name");
+}
+file_put_contents('result/people-list.json', json_encode($db->list_people(), JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
+file_put_contents('result/' . $output , json_encode($db->get_db(), JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
